1cddffb81f2e950a6db2972cddfd0790b3b9b88d
tyehooney <xogns4313@naver.com>
2025-07-09 00:31:46 +0900

    fix: android build warnings

--- app/ui_module/android/app/build.gradle.kts
+++ app/ui_module/android/app/build.gradle.kts
@@ -7,21 +7,21 @@ plugins {
 
 android {
     namespace = "com.example.ui_module"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ui_module"
+        applicationId = "com.stockking.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
